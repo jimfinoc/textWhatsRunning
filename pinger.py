aa755340d3aad500f6ef972161f9ef7dfc14d252
J <jimfinoc@gmail.com>
2015-03-24 21:20:22 -0400

    single line output

--- pinger.py
+++ pinger.py
@@ -4,15 +4,13 @@ import subprocess
 import threading
 import os
 import time
-
+import json
 
 class Pinger(object):
     status = {'alive': [], 'dead': []} # Populated while we are running
     hosts = [] # List of all hosts/ips in our input queue
-    
     # How many ping process at the time.
     thread_count = 4
-    
     # Lock object to keep track the threads in loops, where it can potentially be race conditions.
     lock = threading.Lock()
 #    def clear(self):
@@ -22,19 +20,14 @@ class Pinger(object):
         # Use the system ping command with count of 1 and wait time of 1.
         ret = subprocess.call(['ping', '-c', '1', '-W', '1', ip],
                               stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w'))
-                              
         return ret == 0 # Return True if our ping command succeeds
     
     def pop_queue(self):
         ip = None
-        
         self.lock.acquire() # Grab or wait+grab the lock.
-        
         if self.hosts:
             ip = self.hosts.pop()
-        
         self.lock.release() # Release the lock, so another thread could grab it.
-        
         return ip
 
     def dequeue(self):
@@ -50,8 +43,8 @@ class Pinger(object):
     def start(self):
 #        self.status = {}
         threads = []
-        print "internal status"
-        print self.status
+#        print "internal status"
+#        print self.status
         self.status = {'dead': [], 'alive': []} #this resets the list
         for i in range(self.thread_count):
             # Create self.thread_count number of threads that together will
@@ -64,6 +57,15 @@ class Pinger(object):
         [ t.join() for t in threads ]
         return self.status
 
+
+home_network = "10.0.1."
+ip = []
+for each in range(210,230):
+    ip.append(home_network + str(each))
+print "testing ips"
+print sorted(ip)
+
+
 if __name__ == '__main__':
     while True:
         ping = Pinger()
@@ -73,8 +75,8 @@ if __name__ == '__main__':
         for each in range(210,230):
     #    for each in range(1,256):
             ip.append(home_network + str(each))
-        print "testing ips"
-        print ip
+#        print "testing ips"
+#        print ip
         ping.hosts = ip
     #    ping.hosts = [
     #                  '10.0.1.211','10.0.1.212','10.0.1.213','10.0.1.214','10.0.1.215',
@@ -82,14 +84,21 @@ if __name__ == '__main__':
     #                  'google.com', 'github.com', 'nonexisting', '127.0.1.2', '*not able to ping!*', '8.8.8.8'
     #                  ]
     #    print status
-        print ""
+#        print ""
         status = ping.start()
 #        print "All ips"
 #        print status
 #        print ""
-        print "Alive ips"
-        print status['alive']
-        print ""
-        print "sleeping for 5 seconds"
+        print "Alive ips",
+        print sorted(status['alive'])
+#        print ""
+#        print "sleeping for 5 seconds"
+#        f = open('outputfile.txt', 'w')
+#        f.write(str(status))
+#        f.close()
+
+#        with open("/var/www/index.html","w") as outfile:
+        with open("outputfile.json","w") as outfile:
+            json.dump(status,outfile,indent=4)
         time.sleep(5)
 #        os.system("clear")
